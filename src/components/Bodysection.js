f51c12a135048461beab8aadaa12900cd4205fec
SbuKhoza <sibusisok59@gmail.com>
2024-11-20 15:59:09 +0200

    update

--- src/components/Bodysection.js
+++ src/components/Bodysection.js
@@ -6,12 +6,19 @@ function Bodysection() {
    
     <div className='container'>
         <div className='typography'>
+
+          
+            <h3 id='greating'>Hello, </h3>
+            <h2 id='myname'>I'm S'bu</h2>
+            <h2 id='j-title'>Fullstacks Web & Mobile Developer</h2>
           
 
         </div>
 
         <div className='picture'>
-
+          <div className='profileimage'>
+            <img src='Sibusiso.png' alt='sibusiso'></img>
+        </div>
         </div>
     </div>
    
